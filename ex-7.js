688f52693d2b6622adb11cd9135a219d4473a7ab
sirithanatharathorntv <sirithanatharathorntv@gmail.com>
2023-09-16 16:46:24 +0700

    feat: create new branch pop

--- ex-7.js
+++ ex-7.js
@@ -4,4 +4,6 @@ let subject = "I";
 let verb = "am";
 let object = "A Software Developer";
 
-let sentence;
+// Start coding here
+let sentence = subject + " " + verb + " " + object;
+console.log(sentence);
